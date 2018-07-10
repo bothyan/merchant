44efe05b2a907644d49dead936b09e27b608a530
闫士博 <yanshibo@corp.netease.com>
2018-07-10 19:33:42 +0800

    加成功页

--- xfz/app.js
+++ xfz/app.js
@@ -1,9 +1,65 @@
 //app.js
 App({
-  onLaunch: function () {
-    
-  },
   globalData: {
-    userInfo: null
+    xfz_token:wx.getStorageSync('xfz_token') || "",
+    userInfo: null,
+    host:"https://ssl.zhihuishangjie.cn"
+  },  
+  urlMap:{},
+  onLaunch: function () {
+    this.login();
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  login:function(cb){
+    var that = this;
+    console.log(that.globalData.host)
+    // 登录
+    wx.login({
+      success: function(res){
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res.code);
+        wx.request({
+            url: that.globalData.host+"/app/user/login",
+            header: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            method:"post",
+            data:{
+              code:res.code
+            },
+            success: function(res) {   
+             console.log(res);
+             typeof cb == "function" && cb();
+            }
+        })
+      }
+    })
+  },
+  initUrlMap:function(){
+    var host = this.globalData.host
+    var urlMap = {
+      login: host+"/app/user/login"
+    };
+    this.urlMap = urlMap;
   }
 })
--- xfz/pages/my/my.js
+++ xfz/pages/my/my.js
@@ -4,10 +4,68 @@ const app = getApp()
 
 Page({
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   onLoad: function () {
-    
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    /*var that = this;
+    if(app.globalData.ydbz_token !== ""){
+      that.getIndexData();
+      that.getTagList();
+      that.getLocation();
+      that.getThisWeek();
+    }else{
+      app.login(function(){
+        that.getIndexData();
+        that.getTagList();
+        that.getLocation();
+        that.getThisWeek();                   
+      });
+    }
+    wx.getSetting({
+      success: function(res){
+        if (res.authSetting['scope.userInfo']) {
+          that.setData({
+            buttoncan:false
+          }); 
+        }
+      }
+    })*/
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
   tocard: function() {
     wx.navigateTo({
